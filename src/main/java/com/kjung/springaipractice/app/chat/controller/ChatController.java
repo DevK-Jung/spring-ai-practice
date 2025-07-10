189ccd42a5c69c30a5e0307bb479f729f3f28d5c
김정현 <wjdgus0117@ccmedia.co.kr>
2025-07-10 16:56:10 +0900

    [Feat]: 채팅 스트리밍 응답 API 추가

--- src/main/java/com/kjung/springaipractice/app/chat/controller/ChatController.java
+++ src/main/java/com/kjung/springaipractice/app/chat/controller/ChatController.java
@@ -2,9 +2,11 @@ package com.kjung.springaipractice.app.chat.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/v1/chat")
@@ -16,12 +18,21 @@ public class ChatController {
         this.chatClient = chatClientBuilder.build();
     }
 
-    @GetMapping
+    @GetMapping("/blocking")
     public ChatResponse blocking(String userInput) {
 
         return this.chatClient.prompt()
                 .user(userInput)
                 .call()
                 .chatResponse();
+    }
+
+    @GetMapping(value = "/streaming", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streaming(String userInput) {
+
+        return this.chatClient.prompt()
+                .user(userInput)
+                .stream()
+                .content();
     }
 }
